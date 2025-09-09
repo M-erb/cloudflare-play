d39fb794a02f6696773634d9c6bcd49844f9adc3
merb <merb@mailbolt.com>
2025-09-09 17:59:24 -0600

    feat: query users on page load

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,7 @@
+import type { PageServerLoad } from './$types'
+
+export const load: PageServerLoad = async ({ locals }) => {
+	const users = await locals.db.query.user.findMany()
+	console.log('users: ', users)
+	return { users }
+}
