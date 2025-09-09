8bc294ce7d86a6003ad78a076e7d1cabed2aecc6
merb <merb@mailbolt.com>
2025-09-09 17:58:28 -0600

    feat: add db to svelte locals

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,13 @@
 import type { Handle } from '@sveltejs/kit'
 import * as auth from '$lib/server/auth'
+import { env } from '$env/dynamic/private'
+import { getDb } from '$lib/server/db'
 
 const handleAuth: Handle = async ({ event, resolve }) => {
+	event.locals.db = getDb(event.platform?.env?.DB, env.DATABASE_URL)
 	const sessionToken = event.cookies.get(auth.sessionCookieName)
+
+	console.log('event.platform?.env?.DB', event.platform?.env?.DB)
 
 	if (!sessionToken) {
 		event.locals.user = null
@@ -10,7 +15,7 @@ const handleAuth: Handle = async ({ event, resolve }) => {
 		return resolve(event)
 	}
 
-	const { session, user } = await auth.validateSessionToken(sessionToken)
+	const { session, user } = await auth.validateSessionToken(sessionToken, event.locals.db)
 
 	if (session) {
 		auth.setSessionTokenCookie(event, sessionToken, session.expiresAt)
